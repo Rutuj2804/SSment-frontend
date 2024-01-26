af02e216f11b0ebe5c405a1f3418f86a41902f2e
Rutuj2804 <bokaderutuj36@gmail.com>
2024-01-26 21:09:53 +0530

    pages created for v1.0.0

---
+++ src/pages/authentication/index.tsx
@@ -0,0 +1,3 @@
+export { default as Login } from "./page";
+export { default as ForgotPassword } from "./ForgotPassword";
+export { default as Register } from "./Register";
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,27 @@
 import { Navigate } from "react-router-dom";
 import { Home } from "../pages/home";
 import { Dashboard } from "../pages/dashboard";
+import { Batch } from "../pages/batch";
+import { Result } from "../pages/result";
+import { Test } from "../pages/test";
+import { ForgotPassword, Login, Register } from "../pages/authentication";
 
 export const regularRoutes = [
     { path: "/dashboard", element: <Dashboard /> },
+
+    { path: "/batches", element: <Batch /> },
+
+    { path: "/results", element: <Result /> },
+
+    { path: "/tests", element: <Test /> },
     
     { path: "/", element: <Navigate to="/dashboard" /> },
 ];
 
 export const authRoutes = [
     { path: "/home", element: <Home /> },
+
+    { path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> },
+    { path: "/forgot-password", element: <ForgotPassword /> },
 ]
