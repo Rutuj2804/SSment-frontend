cb27e2d511704f555638794778abedfce25abaf3
Rutuj2804 <bokaderutuj36@gmail.com>
2024-02-15 13:07:32 +0530

    profile & edit profile completed

--- src/pages/profile/EditProfile.tsx
+++ src/pages/profile/EditProfile.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Paper } from "../../components/paper";
+import { Avatar } from "@mui/material";
+import { Input } from "../../library";
+import { useDispatch } from "react-redux";
+import { setBreadcrumps } from "../../store/breadcrumps/slice";
 
 const EditProfile = () => {
-	return <div>EditProfile</div>;
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(
+			setBreadcrumps({
+				name: ["General", "Profile", "Edit Profile"],
+				link: "/edit/:username",
+			})
+		);
+	}, [dispatch]);
+
+	return (
+		<div className="profile__Wrapper">
+			<Paper className="p-3">
+				<h4>Edit Profile</h4>
+				<div className="d-flex my-3">
+					<Avatar sx={{ height: 150, width: 150 }} />
+				</div>
+				<div className="row">
+					<div className="col-lg-6 col-md-6 col-12">
+						<Input label="First Name" placeholder="First Name" />
+					</div>
+					<div className="col-lg-6 col-md-6 col-12">
+						<Input label="Last Name" placeholder="Last Name" />
+					</div>
+					<div className="col-lg-6 col-md-6 col-12">
+						<Input label="Email" placeholder="Email" />
+					</div>
+					<div className="col-lg-6 col-md-6 col-12">
+						<Input
+							type="date"
+							label="Date of Birth"
+							placeholder="Date of Birth"
+						/>
+					</div>
+					<div className="col-lg-6 col-md-6 col-12">
+						<Input type="text" label="State" placeholder="State" />
+					</div>
+					<div className="col-lg-6 col-md-6 col-12">
+						<Input
+							type="text"
+							label="Country"
+							placeholder="Country"
+						/>
+					</div>
+					<div className="col-lg-6 col-md-6 col-12">
+						<Input
+							label="Institute"
+							placeholder="Institute"
+							disabled
+						/>
+					</div>
+				</div>
+			</Paper>
+		</div>
+	);
 };
 
 export default EditProfile;
--- src/pages/profile/page.tsx
+++ src/pages/profile/page.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Paper } from "../../components/paper";
+import { Button, OutlineButton } from "../../library";
+import { useNavigate } from "react-router-dom";
+import { Avatar } from "@mui/material";
+import { DangerousRounded } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { setBreadcrumps } from "../../store/breadcrumps/slice";
 
 const Profile = () => {
-	return <div>Profile</div>;
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(
+			setBreadcrumps({
+				name: ["General", "Profile", "My Profile"],
+				link: "/:username",
+			})
+		);
+	}, [dispatch]);
+
+	const navigate = useNavigate();
+
+	return (
+		<div className="testDetail__Wrapper">
+			<Paper className="p-3">
+				<div className="testDetail__Header">
+					<div className="left">
+						<Avatar sx={{ height: 150, width: 150 }} />
+						<div className="details">
+							<h4>Rutuj Jeevan Bokade</h4>
+							<p>Maharashtra, India</p>
+						</div>
+					</div>
+					<div className="infodetails">
+						<div className="unit">
+							<h6>Role</h6>
+							<p>No Role Assigned</p>
+						</div>
+						<div className="unit">
+							<h6>Date Of Birth</h6>
+							<p>12 Jan, 1987</p>
+						</div>
+						<div className="unit">
+							<h6>Created On</h6>
+							<p>12 Jan, 2024</p>
+						</div>
+						<div className="unit">
+							<h6>Institute</h6>
+							<p>MIT ADT University</p>
+						</div>
+					</div>
+					<div className="actions">
+						<h6>Actions :</h6>
+						<div className="testDetails__Actions">
+							<OutlineButton>Delete Profile</OutlineButton>
+							<Button onClick={() => navigate("/edit/321")}>
+								Edit Profile
+							</Button>
+						</div>
+					</div>
+					<div className="layoutMessage__Message layoutMessage__Danger">
+						<DangerousRounded />
+						<p>
+							You are not authorized to delete your profile.
+							Please contact the Administrator for more
+							information.
+						</p>
+					</div>
+				</div>
+			</Paper>
+		</div>
+	);
 };
 
 export default Profile;
